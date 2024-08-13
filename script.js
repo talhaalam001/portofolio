4b78abba19f5c7733cba0be3a7589d62aa134963
talhaalam001 <152386734+talhaalam001@users.noreply.github.com>
2024-08-13 15:12:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('uploadForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    alert('File upload is not supported in this demo. Please manually upload your file to Google Drive or Dropbox.');
+});
